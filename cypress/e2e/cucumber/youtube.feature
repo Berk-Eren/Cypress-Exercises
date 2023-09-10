925e6dc6d64f66d525c82646f41cef66ad695046
berko <erenb1169@gmail.com>
2023-09-10 13:55:17 +0300

    Youtube scenario is being modified

--- cypress/e2e/cucumber/youtube.feature
+++ cypress/e2e/cucumber/youtube.feature
@@ -3,3 +3,5 @@ Feature: duckduckgo.com
     Scenario: visiting the frontpage
         When I visit "youtube.com"
         Then I should see a search bar
+        Then I should see shorts section
+        Then Enter "pewdepie" into the search bar
--- cypress/e2e/cucumber/youtube/steps.js
+++ cypress/e2e/cucumber/youtube/steps.js
@@ -1,15 +1,30 @@
 const { When, Then } = require('@badeball/cypress-cucumber-preprocessor');
 
 When('I visit {string}', (url) => {
-  cy.visit(`www.${url}`);
+  cy.setCookie('PREF', 'tz=Europe.Berlin');
+  cy.visit(`www.${url}?hl=en`);
+  cy.getCookie('PREF').then((cookie) => {
+    cy.log(cookie);
+  });
 });
 
 Then('I should see a search bar', () => {
-  cy.get('input[type=text]').should(
-    'have.attr',
-    'placeholder',
-    'Search the web without being tracked'
-  );
+  cy.get('input[type=text]').should('have.attr', 'placeholder', 'Search');
+
+  assert.deepEqual({}, {});
+});
+
+Then('Enter {string} into the search bar', (text) => {
+  cy.get('input[type=text]').type(text).click('enter');
+
+  assert.deepEqual({}, {});
+});
+
+Then('I should see shorts section', () => {
+  cy.get('div#dismissible').as('shorts-div');
+  cy.get('@shorts-div')
+    .find('span#title')
+    .should('have.text', 'ShortsTrending');
 
   assert.deepEqual({}, {});
 });
